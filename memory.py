39ebc08247dd3d988fc71e3c44ad99b7be3b94f9
Pranya15 <patelpranya15@gmail.com>
2026-03-17 20:18:18 +0530

    added memory.py

---
+++ memory.py
@@ -0,0 +1,24 @@
+name = "Pranya"
+age = 20
+price = 98.99
+
+age2 = age
+ 
+print(age2)
+print(name)
+
+
+print("my name is:", name)
+print("my age is:", age)
+
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+age = 23
+old = False
+a = None
+print(type(old))
+print(type(a)) 
