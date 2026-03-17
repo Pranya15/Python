4737d860bfd5d8f49f1c122ed23bafe517ab8fd8
Pranya15 <patelpranya15@gmail.com>
2026-03-17 20:18:56 +0530

    added operator.py

---
+++ operator.py
@@ -0,0 +1,55 @@
+a = 46
+b = 79
+print("sum:",a+b) 
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b) #a ^ b
+
+#relational operator
+print( a==b)
+print( a!=b)
+print( a>b)
+print( a<=b)
+
+#assigment opp
+num=10
+#num +=10
+num *=5
+print(num)
+
+
+#logical
+a=5
+b=30
+print(not False)
+print(not(a>b))
+
+val1 = False
+val2 = True
+print(val1 and val2)
+print(val1 or val2)
+
+#type conversion
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(a+b)
+
+
+name = input("enter your name : ")
+print("Welcome" , name)
+
+val = int( input("Enter some values:"))
+print(type(val), val)
+
+
+
+name =input("Enter name:")
+age = int(input("Enter age:"))
+marks = float(input("Enter marks:"))
+print("hello",name)
+print("your age",age)
+print("marks scored",marks)
