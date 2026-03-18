b4f567bb9704c717cd6a3d3e4aeebf3817338e31
Pranya15 <patelpranya15@gmail.com>
2026-03-18 16:44:34 +0530

    added

---
+++ conditional.py
@@ -0,0 +1,27 @@
+#age = 21
+
+#if(age >= 18):
+    #print("can vote and apply for license")
+    #print("can drive")
+
+
+
+
+# light = "green"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("ready")
+
+#     pront("End of code")
+
+
+num = 5
+if(num > 2):
+    print("Greater than 2")
+
+    elif(num>3):
+        print("Greater than 3")
