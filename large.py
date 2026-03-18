7db214de2740efbb7c9b1db0277c3352f419e439
Pranya15 <patelpranya15@gmail.com>
2026-03-18 16:53:29 +0530

    added

---
+++ large.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+c = int(input("Enter the number:"))
+
+if(a>=b and a>=c):
+print("first is largest",a)
+elif(b>=c):
+    print("second is largest",b)
+    else:
+        print("third is largest",c)
