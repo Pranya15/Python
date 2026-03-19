2087ae003b6c199cea6f760a2aed1234bb53b8c7
Pranya15 <patelpranya15@gmail.com>
2026-03-19 15:00:32 +0530

    Added

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# tup = (2, 1, 5, 6)
+# print(type(tup ))
+# print(tup[0])
+# print(tup[1])
+
+#tuple
+# tup = (1,)
+# print(tup)
+# print(type(tup))
+
+# tup=(1) integer
+
+tup = (1,5,7,3)
+print(tup[1:3])
