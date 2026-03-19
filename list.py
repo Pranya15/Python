9772a3b8485b397d456c32252620473871d773fd
Pranya15 <patelpranya15@gmail.com>
2026-03-19 14:20:33 +0530

    added

---
+++ list.py
@@ -0,0 +1,28 @@
+# marks1 = 94.4
+# marks2 = 87.1
+# marks3 = 95.2
+# marks4 = 66.6
+# marks5 = 45.8
+
+#list mutable
+# marks = [94.4 ,87.1, 95.2, 66.6, 45.8]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+
+student = ["karan", 66.9, 17, "mumbai"]
+print(student)
+
+print(student[0])
+student[0] ="karanveer" #mutable
+print(student)
+
+marks = [55, 78, 8, 11, 45]
+print(marks[0 :5])
+marks.append(59)
+print(marks)
+
+ 
