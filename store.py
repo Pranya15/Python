eaf88a4740e23b9f089d176895383e0960c1c742
Pranya15 <patelpranya15@gmail.com>
2026-03-20 15:40:35 +0530

    added

---
+++ store.py
@@ -0,0 +1,43 @@
+dict = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
+}
+
+print(dict)
+
+
+
+
+
+marks = {}
+
+x = int(input("enter phy :"))
+marks.update({"phy" : x})
+
+y = int(input("enter maths :"))
+marks.update({"phy" : y})
+
+z = int(input("enter chem :"))
+marks.update({"phy" : z})
+
+print(marks)
+
+
+
+
+ set = {"python" , "java","C++", "python" ,"javascript" , "java", "python","C++","c"}
+ print(set)
+ print(len(set))
+
+
+
+#strings
+values = {9,9.20,8,"8.0"}
+print(values)
+
+#tuples
+values = {
+    ("float" , 9.0),
+    ("int",9)
+}
+print(values)
