8588c3a9d9b517cb94bcf1f2f3e8aab2eb730a64
Pranya15 <patelpranya15@gmail.com>
2026-03-21 13:45:32 +0530

    added

---
+++ loop.py
@@ -0,0 +1,51 @@
+# count = 1
+# while  count <= 5:
+#     print("hello")
+#if(count ==3):
+   #  break
+  # # continue          skip
+  # #print(count)
+#     count += 1
+# print(count)
+
+
+#1 to 100
+i = 1
+while i<=100
+print(i)
+i +=1
+
+
+#100 to 1
+i = 100
+while i>=1
+print(i)
+i -=1
+
+
+#table
+n = int(input("Enter the no. :"))
+i= 1
+while i<=10
+print(n * i)
+i += 1
+
+
+#print list
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
+
+
+#search
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x = 36
+i = 0
+while i < len(nums):
+    if(nums[i] == x):  
+        print("Found at idx", i)
+    i += 1
+
+
