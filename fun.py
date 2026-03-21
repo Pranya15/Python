64b3203cc6e7e9549a0d0f15b6fa9c4f02f98c60
Pranya15 <patelpranya15@gmail.com>
2026-03-21 17:15:47 +0530

    added

---
+++ fun.py
@@ -0,0 +1,24 @@
+# def cal_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# cal_sum(4,9)
+# cal_sum(35,98)
+
+# def print_hello(): 
+#     print("hello")
+#  output=print_hello()
+#  print(output)
+
+
+#avg of three nos.
+# def cal_avg(a, b, c):
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+# cal_avg(6,8,2)
+
+
+
