2c597566c17db7187fd28703ea7e974c902a7d19
Pranya15 <patelpranya15@gmail.com>
2026-03-21 14:10:17 +0530

    added

---
+++ pratice2.py
@@ -0,0 +1,34 @@
+#sum of n nos.
+n = 5 
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print("total sum=", sum)
+
+
+i= 1
+while i<=n:
+    sum +=i
+    i +=1
+    print("total sum=", sum)
+
+
+#factorial
+
+n = 5
+fact = 1
+i= 1
+while i<=n:
+    fact *=i
+    i +=1
+    print("factorail=", fact)
+
+
+n = 5
+fact = 1
+for i in range(1,n+1):
+    fact*= i
+
+    print("factorial=", fact)
+
+
