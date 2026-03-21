566d7811907b17df25df301d5df830a80c3d9dc4
Pranya15 <patelpranya15@gmail.com>
2026-03-21 13:45:56 +0530

    added

---
+++ loop2.py
@@ -0,0 +1,33 @@
+veggies = ["potato" , "brijal", "ladyfinger", "cucumber"]
+for val in veggies:
+    print(val)
+
+
+
+tup = (1, 4 ,6 ,9 ,2 ,3 ,7)
+for num in tup:
+    print(num)
+
+
+
+str ="hiiiieieeieie"
+for char in str:
+    print(char)
+else:
+    print("END")
+
+#print num
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+for val in nums:
+    print(val)
+
+
+#search
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49]
+x = 49
+idx = 0
+for val in nums:
+    if(el == x):
+        print("no found at idx",)
+        idx +=1
+    
