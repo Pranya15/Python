34e46cdef71ec40944d9d521770bbbe580bd752c
Pranya Patel <patelpranya15@gmail.com>
2026-04-12 10:30:29 +0530

    added

---
+++ module.py
@@ -0,0 +1,8 @@
+def myFunc():
+    print("Hello India")
+
+if __name__ == "__main__":
+    #if this code is directly executed by running the file its present in
+    print("We are direcly running this code")
+    myFunc()
+    print(__name__)
