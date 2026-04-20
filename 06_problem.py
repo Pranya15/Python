aef8e33da5dfd34e32af107be136b49b1319e139
Pranya Patel <patelpranya15@gmail.com>
2026-04-20 17:39:00 +0530

    Added

---
+++ 06_problem.py
@@ -0,0 +1,6 @@
+name = input("Enter name: ") 
+marks = int(input("Enter marks: "))
+phone = int(input("phone number: "))
+
+s = "the name of student is {} , her marks are {} and phone number is {}".format(name, marks, phone)
+print(s)  
